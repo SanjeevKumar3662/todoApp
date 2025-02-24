999b45a7082bdf1edad263a91efd8136a78e9479
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-02-24 21:08:58 +0530

    now items can be added to the DOM

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+alert("new Start");
 const addBtn = document.querySelector("#addBtn");
 
-addBtn.addEventListener("click", (e) => {
+//creates list item for DOM
+const createListItem = (inputVal) => {
+  //creating li and appending a textNode within it
+  const li = document.createElement("li");
+  li.classList.add("list-item");
+  li.appendChild(document.createTextNode(inputVal));
+
+  //creating the x button
+  const btn = document.createElement("button");
+  const icon = document.createElement("i");
+  icon.classList.add("fa");
+  icon.classList.add("fa-x");
+  btn.appendChild(icon);
+
+  //appending the x/delete button to li
+  li.appendChild(btn);
+
+  //list element has been created and returned
+  return li;
+};
+
+const addElementToDOM = (e) => {
+  //this will prevent the form from submitting
   e.preventDefault();
-  alert("js working");
-});
+
+  //getting the value form the input form
+  const inputValue = document.querySelector("#mainInput").value;
+  // console.log(inputValue);
+
+  //calling the creatListItem function this will create the list item
+  const liItem = createListItem(inputValue);
+
+  //adding list item to the DOM
+  const parentUl = document.querySelector("#list");
+  parentUl.appendChild(liItem);
+};
+
+addBtn.addEventListener("click", addElementToDOM);
